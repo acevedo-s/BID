2970e630197b433e669d35b7dd419053ed28afdc
acevedo-s <santiagodanielacevedo@gmail.com>
2025-01-14 14:27:04 +0100

    .

--- Ising-Square/FCI/bcompute.sh
+++ Ising-Square/FCI/bcompute.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-T_list=($(seq 1.8 0.1 3))
+# T_list=($(seq 1.8 0.1 3))
 # T_list+=($(seq 2.21 0.01 2.39))
-# L_list=({30..100..10})
+T_list=(1.7 1.8 2.3 3 4)
+L_list=({30..100..10})
 M_flags=(1)
-L_list=(30)
+# L_list=(30)
 global_flags=(1)
 
 for global_flag in "${global_flags[@]}"
